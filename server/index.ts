5a9df952074fbe38dbca7665d043cc3de44de557
Aditi <aditiraj0205@gmail.com>
2026-01-04 18:30:15 +0530

    fixes

--- server/index.ts
+++ server/index.ts
@@ -75,7 +75,7 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 10000;
   server.listen(PORT, () => {
     log(`Server is running at http://localhost:${PORT}`);
   });
