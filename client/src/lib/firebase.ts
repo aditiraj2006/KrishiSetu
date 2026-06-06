df763672489e0fac3af1faa619eeae6b1605d09b
Mᴇɢʜ Pᴀᴛᴇʟ <meghpatel0009@gmail.com>
2026-06-06 17:09:32 +0530

    resolve some bugs made by me

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -10,7 +10,7 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "placeholder.appspot.com",
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "000000000000",
   appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:000000000000:web:0000000000000000",
-  measurementId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "placeholder-measurement-id",
 };
 
 const requiredFirebaseValues = [
